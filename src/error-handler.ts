051a16047f69f3612ea24c84a3b54c34592b0271
Vladimir Rudenko <ruden.v.a@gmail.com>
2017-10-06 07:30:13 +0300

    Error-handler: replace empty message with default http status message by status code

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -8,10 +8,13 @@ export const errorHandler = function (debugMode) {
 
         const statusCode = err.statusCode || 500;
 
+        if (error.message === undefined) {
+            error.message = http.STATUS_CODES[statusCode];
+        }
+
         if (err.code !== undefined) {
             error.code = err.code;
         }
-
 
         if (err.errors !== undefined) {
             error.errors = err.errors;
@@ -22,7 +25,7 @@ export const errorHandler = function (debugMode) {
         if (debugMode && statusCode >= 500) {
             console.error(time, err.stack);
         } else {
-            console.log(time, '- [error]', error.message || http.STATUS_CODES[statusCode]);
+            console.log(time, '- [error]', error.message);
         }
 
         res.status(statusCode).json(error);
